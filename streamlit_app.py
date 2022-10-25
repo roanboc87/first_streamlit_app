d712b71b5bc330ba4954e61e3cb5f23796e7a95d
roanboc87 <68288369+roanboc87@users.noreply.github.com>
2022-10-26 09:19:47 +1030

    Snowflake query

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,18 @@ streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
 
+# Connect to Snowflake with secrets configured in streamlit secrets config
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+# Get account data
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+# Get data from tables
+my_cur.execute("SELECT * FROM fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
